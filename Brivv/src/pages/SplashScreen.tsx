c927f78daba0a82287ae4be5d0eb2066bc76bc73
Lawrence-afi <Okonlawrence00@gmail.com>
2026-04-14 13:45:00 -1100

    merge landing page

--- Brivv/src/pages/SplashScreen.tsx
+++ Brivv/src/pages/SplashScreen.tsx
@@ -1,10 +1,33 @@
-import { useEffect, useState } from "react";
-import { useNavigate } from "react-router";
+import Hero from "../Components/Hero";
+import Features from "../Components/Features";
+import Showcase from "../Components/Showcase";
+import PropertyReels from "../Components/PropertyReels";
+import Journey from "../Components/Journey";
+import SmartMaching from "../Components/SmartMaching";
+import UtilityPayments from "../Components/UtilityPayments";
+import MarketPlace from "../Components/MarketPlace";
+import MetricsBanner from "../components/MetricsBanner";
+import TestimonialSection from "../components/TestimonialSection";
+import CTASection from "../components/CTASection";
+import Footer from "../components/Footer";
 
 const HomeScreen = () => {
-  return (<p>
-   home screen
- </p>)
+  return (
+    <>
+      <Hero />
+      <Features />
+      <Showcase />
+      <PropertyReels />
+      <Journey />
+      <SmartMaching />
+      <UtilityPayments />
+      <MarketPlace />
+      <MetricsBanner />
+      <TestimonialSection />
+      <CTASection />
+      <Footer />
+    </>
+  );
 };
 
 export default HomeScreen;
